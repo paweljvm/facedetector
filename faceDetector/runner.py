2fdea0538fde9429a80fb0119b3fa09445145dfa
pawel <pcchack@o2.pl>
2019-09-25 23:05:27 +0200

    Runner added uploader controller

--- faceDetector/runner.py
+++ faceDetector/runner.py
@@ -2,6 +2,7 @@ from config import config
 from init.init import app
 import train.train_controller
 import detect.detect_controller
+import uploader.upload_controller
 @app.route("/")
 def index():
     return "<b>FACE DETECTOR</b>"
